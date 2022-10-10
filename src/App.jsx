d01f2e80cd51b2fb0d3ff2011711169b55f46a6a
SULAJSJS <98090790+SULAJSJS@users.noreply.github.com>
2022-10-10 17:01:20 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,57 @@ import { Reorder } from 'framer-motion';
 import './App.scss';
 import { Clear } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import UploadItems from './components/UploadItems';
 
 function App() {
+  // const [image, setImage] = useState([
+  //   {
+  //     id: 0,
+  //     order: 0,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 1,
+  //     order: 3,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 2,
+  //     order: 6,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 3,
+  //     order: 5,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 4,
+  //     order: 1,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 5,
+  //     order: 8,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 6,
+  //     order: 7,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 7,
+  //     order: 4,
+  //     url: null,
+  //   },
+  //   {
+  //     id: 8,
+  //     order: 2,
+  //     url: null,
+  //   },
+  // ]);
   const [image, setImage] = useState([
     {
       id: 0,
@@ -15,41 +62,45 @@ function App() {
     },
     {
       id: 1,
-      order: 1,
-      url: null,
-    },
-    {
-      id: 2,
-      order: 2,
-      url: null,
-    },
-    {
-      id: 3,
       order: 3,
       url: null,
     },
     {
-      id: 4,
-      order: 4,
-      url: null,
-    },
-    {
-      id: 5,
+      id: 2,
       order: 5,
       url: null,
     },
     {
+      id: 3,
+      order: 7,
+      url: null,
+    },
+    {
+      id: 4,
+      order: 8,
+      url: null,
+    },
+    {
+      id: 5,
+      order: 4,
+      url: null,
+    },
+    {
       id: 6,
-      order: 6,
+      order: 2,
       url: null,
     },
     {
       id: 7,
-      order: 7,
+      order: 1,
+      url: null,
+    },
+    {
+      id: 8,
+      order: 6,
       url: null,
     },
   ]);
-  const [picture, setPicture] = useState(null);
   const [isHovering, setIsHovering] = React.useState(false);
   const imageHandleChange = (e, el) => {
     if (e.target.file) {
@@ -61,6 +112,7 @@ function App() {
         return item.id === el.id
           ? {
               id: item.id,
+              order: item.order,
               url: URL.createObjectURL(file),
             }
           : item;
@@ -83,29 +135,15 @@ function App() {
     );
   };
 
-  const uploadImage = (e) => {
-    if (e.target.file) {
-      return;
-    }
-    setPicture(URL.createObjectURL(e.target.files[0]));
+  const todoGeneral = (id, el) => {
+    // setImage(
+    //   image.map((item) => {
+    //     return item.id === el.id
+    //       ? item?.url[0] = el?.url
+    //       : item;
+    //   }),
+    // );
   };
-
-  const todoGeneral = (id, el, picture) => {
-    setPicture(el.url);
-    setImage(
-      image.map((item) => {
-        return item.id === el.id
-          ? {
-              id: item.id,
-              order: item.order,
-              url: picture,
-            }
-          : item;
-      }),
-    );
-  };
-
-
 
   // Button styles
 
@@ -129,13 +167,29 @@ function App() {
 
   return (
     <div className="App">
-      <Reorder.Group as="div" axis="y" values={image} onReorder={setImage}>
-        <div className="upload">
-          <Reorder.Item
+      <Box component='div' className="upload">
+        <UploadItems
+          todoGeneral={todoGeneral}
+          deleteImageHandler={deleteImageHandler}
+          imageHandleChange={imageHandleChange}
+          image={image}
+          setImage={setImage}
+        />
+      </Box>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+{/* <Reorder.Item
             key={picture}
             value={picture}
             as="label"
-            htmlFor='input'
+            htmlFor="input"
             className="upload-content">
             {picture ? (
               <span>
@@ -190,21 +244,4 @@ function App() {
             accept="images/*"
             onChange={(e) => uploadImage(e)}
             multiple
-          />
-          <UploadItems
-            todoGeneral={todoGeneral}
-            deleteImageHandler={deleteImageHandler}
-            imageHandleChange={imageHandleChange}
-            image={image}
-            setImage={setImage}
-            picture={picture}
-            setPicture={setPicture}
-            uploadImage={uploadImage}
-          />
-        </div>
-      </Reorder.Group>
-    </div>
-  );
-}
-
-export default App;
+          /> */}
--- src/components/UploadItems.jsx
+++ src/components/UploadItems.jsx
@@ -4,17 +4,10 @@ import { styled } from '@mui/material/styles';
 import { Button } from '@mui/material';
 import { Reorder } from 'framer-motion';
 
-const UploadItems = ({
-  todoGeneral,
-  imageHandleChange,
-  deleteImageHandler,
-  image,
-  setImage,
-  picture,
-  setPicture,
-}) => {
+const UploadItems = ({todoGeneral, imageHandleChange, deleteImageHandler, image, setImage,}) => {
   const [isHovering, setIsHovering] = React.useState(false);
-
+  const [currentImage, setCurrentImage] = React.useState(null);
+  // =========== button style
   const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText('#0063cc'),
     backgroundColor: '#0063cc',
@@ -23,7 +16,8 @@ const UploadItems = ({
       backgroundColor: '#498fd6',
     },
   }));
-
+  // =========== Finished button style
+  //=========== Hover
   const handleMouseEnter = () => {
     setIsHovering(true);
   };
@@ -31,14 +25,55 @@ const UploadItems = ({
   const handleMouseLeave = () => {
     setIsHovering(false);
   };
+  // =========== finished hover
+
+  // =========== sort
 
   const sorted = (a, b) => {
     return a.url === null && b.url !== null ? 1 : -1;
   };
+  // =========== Finished sort
+
+  // =========== DND
+
+  const dragStartHandler = (e, el) => {
+    console.log('drag', el);
+    setCurrentImage(el);
+  };
+  const dragEndHandler = (e) => {};
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+  };
+  const dropHandler = (e, el) => {
+    console.log('drop', el);
+    e.preventDefault();
+    setImage(
+      image.map((item) => {
+        if (item.id === el.id) {
+          return { ...item, order: currentImage.order, id: item.id };
+        }
+        if (item.id === currentImage.id) {
+          return { ...item, order: el.order, id: item.id };
+        }
+        return item;
+      }),
+    );
+  };
+  const sortImages = (a, b) => {
+    if (a.order > b.order) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
+  // =========== Finished DND
+
+  
 
   return (
     <div className="upload-content-items">
-      {image?.sort(sorted).map((el, id) => (
+      {image?.sort(sortImages, sorted).map((el, id) => (
         <>
           <input
             id={el.id}
@@ -47,70 +82,119 @@ const UploadItems = ({
             accept="images/*"
             onChange={(e) => imageHandleChange(e, el)}
           />
-          <Reorder.Item
+          <label
             key={el.id}
-            value={el}
-            as="label"
             htmlFor={el.id}
             className="item"
             style={
-              el.url
+              el.url && id == 0
                 ? {
                     boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+                    width: '13rem',
+                    height: '13rem',
+                    marginLeft: '-14rem',
+                    position: 'absolute'
                   }
+                : id === 0
+                ? { width: '13rem', height: '13rem', marginLeft: '-14rem', position: 'absolute' }
                 : {
                     boxShadow: 'none',
                     cursor: 'pointer',
-
                   }
             }>
             {el.url ? (
-              <span className="span" style={{display: 'flex', flexDirection: 'column'}}>
+              <span className="span" style={{ display: 'flex', flexDirection: 'column' }}>
                 <Clear
                   onClick={(e) => deleteImageHandler(e, el)}
-                  sx={{
-                    color: '#fff',
-                    zIndex: 10,
-                    ml: '4.5rem',
-                    mt: '-1.4rem',
-                    backgroundColor: isHovering ? 'red' : '#000',
-                    cursor: 'pointer',
-                    transition: '0.4s',
-                  }}
+                  sx={
+                    id === 0
+                      ? {
+                          color: '#fff',
+                          zIndex: 10,
+                          ml: '11.5rem',
+                          mt: '-1.4rem',
+                          backgroundColor: isHovering ? 'red' : '#000',
+                          cursor: 'pointer',
+                          transition: '0.4s',
+                        }
+                      : {
+                          color: '#fff',
+                          zIndex: 10,
+                          ml: '4.5rem',
+                          mt: '-1.4rem',
+                          backgroundColor: isHovering ? 'red' : '#000',
+                          cursor: 'pointer',
+                          transition: '0.4s',
+                        }
+                  }
                   onMouseEnter={handleMouseEnter}
                   onMouseLeave={handleMouseLeave}
                   className="delete"
                 />
-                <img
-                  className="uploaded-image"
-                  style={{
-                    width: '6rem',
-                    height: '6rem',
-                    borderRadius: '1.4rem',
-                    marginTop: '-1.5rem',
-                  }}
-                  src={el?.url}
-                  alt=""
-                />
+                <div
+                  draggable={true}
+                  onDragStart={(e) => dragStartHandler(e, el)}
+                  onDragLeave={(e) => dragEndHandler(e)}
+                  onDragEnd={(e) => dragEndHandler(e)}
+                  onDragOver={(e) => dragOverHandler(e)}
+                  onDrop={(e) => dropHandler(e, el)}>
+                  <img
+                    className="uploaded-image"
+                    style={
+                      id == 0
+                        ? {
+                            width: '13rem',
+                            height: '13rem',
+                            marginRight: '3rem',
+                            borderRadius: '1.4rem',
+                            marginTop: '-1.6rem',
+                            objectFit: 'cover'
+                          }
+                        : {
+                            width: '6rem',
+                            height: '6rem',
+                            borderRadius: '1.4rem',
+                            marginTop: '-1.5rem',
+                            objectFit: 'cover'
+                          }
+                    }
+                    src={el?.url}
+                    alt=""
+                  />
+                </div>
                 <ColorButton
-                  onClick={() => todoGeneral(id, el, picture)}
+                  onClick={() => todoGeneral(id, el)}
                   className="button"
                   variant="contained"
-                  sx={{
-                    fontSize: 7.8,
-                    borderRadius: '1.7rem',
-                    mt: '-32px',
-                    width: '5.4rem',
-                    ml: '0.3rem',
-                  }}>
+                  sx={
+                    id == 0
+                      ? {
+                          fontSize: 14,
+                          borderRadius: '1.7rem',
+                          mt: '-2.4rem',
+                          width: '11rem',
+                          ml: '1rem',
+                        }
+                      : {
+                          fontSize: 7.8,
+                          borderRadius: '1.7rem',
+                          mt: '-32px',
+                          width: '5.4rem',
+                          ml: '0.3rem',
+                        }
+                  }>
                   <p style={{ whiteSpace: 'nowrap' }}>Сделать главной</p>
                 </ColorButton>
               </span>
             ) : (
-              <img src="/iconmonstr-picture-thin.svg" alt="Image" id="item-icon" />
-              
+              <img
+                style={id == 0 ? { width: '9rem', height: '9rem', marginTop: '0.5rem' } : {}}
+                src="/iconmonstr-picture-thin.svg"
+                alt="Image"
+                id="item-icon"
+              />
             )}
-          </Reorder.Item>
+          </label>
         </>
       ))}
     </div>
